94c1b50adf6ac100e6eb9fdf51dd12691862edc3
iszmaelIbrahim <77146724+iszmaelIbrahim@users.noreply.github.com>
2021-06-25 09:47:51 +0800

    commit

--- day4/sms.py
+++ day4/sms.py
@@ -6,8 +6,8 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 
-# export TWILIO_ACCOUNT_SID='AC63742f61dcb3033a5d473d9b23d238cc'
-# export TWILIO_AUTH_TOKEN='e52cce9ad83ed8e66efba8d1491e498a'
+# export TWILIO_ACCOUNT_SID='TWILIO_ACCOUNT_SID'
+# export TWILIO_AUTH_TOKEN='TWILIO_AUTH_TOKEN'
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
